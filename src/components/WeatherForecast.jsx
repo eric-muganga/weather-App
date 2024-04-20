ff93518ddc324c5bfebbbba2d868c8c3212f74b1
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2024-04-20 14:02:29 +0200

    forecast update

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -20,7 +20,7 @@ function WeatherForecast() {
   const { forecast } = cityData.weather;
 
   return (
-    <div className="max-w-3xl mx-auto p-4 ">
+    <div className="max-w-5xl mx-auto p-4 ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {forecast.forecastday.map((day, index) => (
           <div
